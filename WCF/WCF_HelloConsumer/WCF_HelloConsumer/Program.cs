c7e62bdca5ced9641dc402ec8713b3b2021bcd45
code4fun2025 <code4fun2025@gmail.com>
2020-02-09 22:21:02 -0800

    Create WCF Hello Consumer to consume the WCF Hello Service

---
+++ WCF/WCF_HelloConsumer/WCF_HelloConsumer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WCF_HelloConsumer.Proxy_Hello;
+
+namespace WCF_HelloConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Proxy_Hello.HelloServiceClient proxy = new HelloServiceClient("BasicHttpBinding_IHelloService");
+            string str = proxy.HelloMessage();
+            Console.WriteLine(" Got Message from HelloService:" + str);
+            Console.ReadLine();
+        }
+    }
+}
